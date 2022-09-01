0755d7a8caab6ca4b63038375f4c78be177478dc
JianboYang <jyan0173@student.monash.edu>
2022-09-01 16:35:43 +0800

    Add project files.

---
+++ FIT5032_WEEK5_MyViewModel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FIT5032_WEEK5_MyViewModel.MvcApplication" Language="C#" %>
---
+++ FIT5032_WEEK5_MyViewModel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_WEEK5_MyViewModel.Startup))]
+namespace FIT5032_WEEK5_MyViewModel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
